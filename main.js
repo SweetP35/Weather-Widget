c522b95de1f8483846299cbd06824c424cbf671f
Lena <lena-mishenkova-1998@mail.ru>
2024-09-27 17:58:59 +0300

    Adding icons

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import conditions from "./conditions.js";
-console.log(conditions);
+
 
 
 const apiKey = '3329ee16dde44cc7988143613242509';
@@ -18,22 +18,20 @@ function showError(errorMessage) {
 }
 
 function showCard(cardDetails) {
-    const { name, country, temperature, condition } = cardDetails
+    const { name, country, temperature, condition, images } = cardDetails
     // Отображаем полученные данные в карточку
     //Разметка для карточки
     const html = `<div class="card">
         <div class="card-city">${name} <span>${country}</span></div>
         <div class="card-weather">
             <div class="card-value">${temperature}<sup>°c</sup></div>
-            <img class="card-img" src="./img/example.png" alt="Weather">
+            <img class="card-img" src="${images}" alt="Weather">
         </div>
         <div class="card-description">${condition}</div>
     </div>`;
     //Отображаем карточку на странице
     header.insertAdjacentHTML('afterend', html);
 }
-
-
 
 async function getWeather(city) {
     const url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`;
@@ -54,12 +52,16 @@ form.onsubmit = async function (e) {
     } else {
         removeCard();
         const info = conditions.find((el) => el.code === data.current.condition.code);
-        const condition = data.current.is_day ? info.languages[23]['day_text'] : info.languages[23]['night_text']
+        const filePath = './img/' + (data.current.is_day ? 'day' : 'night') + '/';
+        const fileName = (data.current.is_day ? info.day : info.night) + '.png';
+        const imgPath = filePath + fileName;
+        
         const cardDetails = {
             name: data.location.name,
             country: data.location.country,
             temperature: data.current.temp_c,
-            condition: condition
+            condition: data.current.is_day ? info.languages[23]['day_text'] : info.languages[23]['night_text'],
+            images: imgPath
         }
         showCard(cardDetails);
     }
